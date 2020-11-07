8cbb03ad0b786ed97cf077b06e362d0a64748660
RoshanCS <roshan.cs790@gmail.com>
2020-11-07 16:49:13 +0530

    Create numpy_test.py
    numpy matrices transpose

---
+++ numpy_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.matrix('1, 2; 3, 4; 5, 6')
+b = np.array([[2, 3],
+             [4, 5],
+             [6, 7]])
+
+c = np.matrix([[7, 8],
+               [9, 11],
+               [1, 2]])
+print(c)
+#print(a, "\n", a.T)
