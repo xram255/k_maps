5d5bf075a5629b0a7046548ac4e4768b4de124a0
RoshanCS <roshan.cs790@gmail.com>
2020-11-08 14:43:55 +0530

    scan 8
    Horizontal two row scanner completed

--- km_test1.py
+++ km_test1.py
@@ -1,26 +1,21 @@
 import numpy as np
 from time import sleep as delay
 
-r = np.array([[0, 0, 0, 0],
-              [0, 0, 0, 0],
-              [1, 1, 1, 0],
-              [0, 1, 0, 0]])
+r = np.array([[1, 0, 1, 1],
+              [1, 1, 1, 1],
+              [1, 1, 1, 1],
+              [1, 0, 1, 1]])
+
+r_str = np.array([['abcd', 'abcD', 'abCD', 'abCd'],
+                  ['aBcd', 'aBcD', 'aBCD', 'aBCd'],
+                  ['ABcd', 'ABcD', 'ABCD', 'ABCd'],
+                  ['Abcd', 'AbcD', 'AbCD', 'AbCd']])
+
 
 c = r.T
-
+#print(r_str[0][3])
+#print(r, '\n\n', c)
 #--Scan All--
-'''
-inc1 = 0
-inc2 = 0
-for indx1 in r:
-    inc1 += 1
-    for indx2 in indx1:
-        inc2 += 1
-        if(indx2 == 1):
-            print(inc1, inc2)
-    inc2 = 0
-        #delay(.5)  
-'''
 
 def scan_mt(inmt):
     inc1 = 0
@@ -32,11 +27,24 @@ def scan_mt(inmt):
             if(indx2 == 1):
                 print(inc1, inc2)
         inc2 = 0
-
+'''
 scan_mt(r)
-print(" --- ")
+print("---")
 scan_mt(c)
+print('===')
+'''
 #--Scan 8 - hor
+def scan8hor():
+    outlist = []
+    num_po = 0
+    for num in range(4):
+        num_po += 1
+        if num_po > 3:
+            num_po = 0
+        if(r[num].all() and r[num_po].all()):
+            outlist.append(num)
+            #print(num, num_po, 'all true')
+    return outlist
 
 #--scan 8 - ver
 
@@ -49,3 +57,16 @@ scan_mt(c)
 #--scan 2 hor
 
 #--scan 2 ver
+
+
+# Returning multiple values
+def mult_return():
+    gr = "answer is "
+    v = 54
+    return [gr, v]
+
+
+
+if __name__ =="__main__":
+    #print(mult_return()[1])
+    print(scan8hor())
--- numpy_test.py
+++ numpy_test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import km_test1 as kmt
+
 '''
 a = np.matrix('1, 2; 3, 4; 5, 6')
 b = np.array([[2, 3],
@@ -38,15 +40,25 @@ def scan_str2(stra, strb):
 
 def scan_str4(stra, strb, strc, strd):
     strl = scan_str2(stra, strb)
-    print(strl)
+    #print(strl)
     strr = scan_str2(strc, strd)
-    print(strr)
+    #print(strr)
     outstr = scan_str2(strl, strr)
+    return outstr
+
+def  scan_str8(st, a):
+    b = a + 1
+    if b > 3:
+        b = 0
+    stru = scan_str4(st[a][0], st[a][1], st[a][2], st[a][3])
+    strd = scan_str4(st[b][0], st[b][1], st[b][2], st[b][3])
+    outstr = scan_str2(stru, strd)
     return outstr
 
 #print(scan_str4(r3c1, r3c2, r4c1, r4c2))
 #print(scan_str2(r3c1, r3c2))
 
+'''
 arr1 = np.array([[1, 0, 1, 1],
                  [1, 1, 1, 1]])
 
@@ -55,3 +67,13 @@ if(arr1[0].all() == True):
     print("all values are true ")
 else:
     print("all values are not true")
+'''
+
+#print(kmt.scan8hor())
+print(len(kmt.scan8hor()))
+
+print(scan_str8(kmt.r_str, kmt.scan8hor()[0]))
+
+#print(kmt.r_str[0])
+
+
