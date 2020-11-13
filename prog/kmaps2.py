9b2d4632f1f5252dd1c8fc47dd549988c03b1a90
RoshanCS <roshan.cs790@gmail.com>
2020-11-13 08:59:09 +0530

    Added read functions

--- prog/kmaps2.py
+++ prog/kmaps2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #-- Constant values--
-X = 2
+X = 32 # 2^6, 
 
 #                   C0      C1      C2      C3
 ROW = np.array([['abcd', 'abcD', 'abCD', 'abCd'],  # R1
@@ -13,48 +13,63 @@ COL = ROW.T
 #-------------------------
 # -- Inputs----
 row = np.array([[0, 0, 0, 0],
-                [0, X, 0, 0],
+                [1, X, 0, 0],
                 [1, 1, 1, X],
-                [0, 0, 1, 0]])
+                [1, 0, 1, 0]])
 
 col = row.T
 #-----------------------------
-# --- Read from Matrix ---
-def read8(indata, starting): # indata=Row or Column, Starting=Starting cell Row OR Column number
-    start_plus_one = 0
-    if starting >= 3: # row(or column) length = 4
-        start_plus_one = 0
-    else:
-        start_plus_one = starting + 1 # Neibouring row(column)
-    return (list(indata[starting]) + list(indata[start_plus_one]))
-# ------------------------
+outv = 0
+for inc1 in row:
+    for inc2 in inc1:
+        outv = outv + inc2
+    print(outv & 0b011111)
 
-#=====Scan Strings=========
+# --- Read from Matrix ---
+def read2(indata, start1, start2): # indata = ROW or COL, start1 = row, start2 = col
+    start2_plus1 = start2 + 1
+    if start2_plus1 >= 3:
+        start2_plus1 = 0
+    return [indata[start1][start2], indata[start1][start2_plus1]]
+
+def read4sqr(indata, start1, start2):# indata = ROW or COL, start1 = row, start2 = col
+    outstring1 = read2(indata, start1, start2)
+    outstring2 = read2(indata, start1 + 1, start2)
+    return outstring1 + outstring2
+
+def read8(indata, start): # indata=Row or Column, Starting=Starting cell Row OR Column number
+    start_plus1 = start + 1
+    if start_plus1 >= 3: # row(or column) length = 4
+        start_plus1 = 0
+    return (list(indata[start]) + list(indata[start_plus1]))
+# -------------End of read from matrix -----------
+
+#=====compare Strings=========
 #strns = ['abcd', 'abcD', 'abCD', 'abCd', 'aBcd','aBcD', 'aBCD', 'aBCd']
 
-def scan2str(inlist): # Scan list of two strings, two strings has to be the same lenght
+def comp2str(inlist): # compare list of two strings, two strings has to be the same lenght
     outstr = ''
     for inc in range (len(inlist[0])): # increment for num of letters in first string in list
         if (inlist[0][inc] == inlist[1][inc]): # compare each letter 
             outstr += inlist[0][inc] # added to outstring if matched 
     return outstr
 
-def scan4str(inlist): # Scan list of 4 strings 
+def comp4str(inlist): # compare list of 4 strings 
     inlist1 = [inlist[0], inlist[1]] # creates list 1
     inlist2 = [inlist[2], inlist[3]]
-    strl = scan2str(inlist1) # compare
-    strr = scan2str(inlist2)
-    return scan2str([strl, strr])
+    strl = comp2str(inlist1) # compare
+    strr = comp2str(inlist2)
+    return comp2str([strl, strr])
 
-def scan8str(inlist): # Scan list of 8 strings
+def comp8str(inlist): # compare list of 8 strings
     inlist1 = [inlist[0], inlist[1], inlist[2], inlist[3]]
     inlist2 = [inlist[4], inlist[5], inlist[6], inlist[7]]
-    str1 = scan4str(inlist1)
-    str2 = scan4str(inlist2)
-    return scan2str([str1, str2])
+    str1 = comp4str(inlist1)
+    str2 = comp4str(inlist2)
+    return comp2str([str1, str2]) # returns similar characters only
 
 
-#=====End of Scan Strings==========       
+#=====End of comp Strings==========       
 
 
 
